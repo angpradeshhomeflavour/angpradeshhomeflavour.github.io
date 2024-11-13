07cd3e5e07b9e022de54cc2637b1d89eff76e9c0
angpradeshbgp <angpradeshbgp31@gmail.com>
2024-11-13 17:22:19 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function orderWhatsApp() {
+    const whatsappNumber = '+919508706378'; // replace with actual number
+    const message = "Hello, I would like to place an order for Desi Home Flavour products.";
+    window.open(https://wa.me/${whatsappNumber}?text=${encodeURIComponent(message)}, '_blank');
+}
+
+function orderPhone() {
+    const phoneNumber = '+919508706378'; // replace with actual number
+    window.location.href = tel:${phoneNumber};
+}
+
